ffe31573bbcd12bcc5faca6234485987bd761c12
andreachirico11 <andreachirico11@gmail.com>
2022-05-10 07:03:28 +0200

    CU-2wzfxw3

---
+++ versionUpdater.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const filePath = 'version.json';
+
+try {
+  const versionFile = JSON.parse(fs.readFileSync(filePath));
+  versionFile.number = versionFile.number + 1;
+  fs.writeFileSync(filePath, JSON.stringify(versionFile));
+} catch (error) {
+  console.log('ERROR UPDATING VERSION: ', error);
+}
