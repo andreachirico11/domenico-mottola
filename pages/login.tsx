3ecfed9b5768723e8d5e1c1d777f2cef9c571606
andreachirico11 <andreachirico11@gmail.com>
2022-05-11 09:18:36 +0200

    feat: first component setup CU-2xax27q

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next'
+import { LoginForm } from '../components'
 
 const Login: NextPage = () => {
-  return <div>Login page</div>
+  return (
+    <div className="grid h-screen w-screen place-content-center bg-stone-100">
+      <LoginForm />
+    </div>
+  )
 }
 
 export default Login
